2f16014a7a7355ba4e846fcebc2c5e705ec8877b
CyMiller757 <80051707+CyMiller757@users.noreply.github.com>
2021-09-07 16:19:58 -0400

    worked on logic, finished up

--- Project 4/Project 4/Form1.Designer.cs
+++ Project 4/Project 4/Form1.Designer.cs
@@ -39,16 +39,28 @@ namespace Project_4
             this.p2Score = new System.Windows.Forms.TextBox();
             this.p2Roll = new System.Windows.Forms.Button();
             this.p1Roll = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.gameStarter = new System.Windows.Forms.Button();
+            this.dice1Display = new System.Windows.Forms.PictureBox();
+            this.dice2Display = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.turnMessage = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dice1Display)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2Display)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(101, 99);
+            this.pictureBox1.Location = new System.Drawing.Point(-6, 110);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(682, 94);
+            this.pictureBox1.Size = new System.Drawing.Size(534, 102);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -56,7 +68,7 @@ namespace Project_4
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label1.Location = new System.Drawing.Point(871, 24);
+            this.label1.Location = new System.Drawing.Point(22, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 20);
             this.label1.TabIndex = 1;
@@ -66,7 +78,7 @@ namespace Project_4
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.Location = new System.Drawing.Point(871, 447);
+            this.label2.Location = new System.Drawing.Point(21, 14);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(69, 20);
             this.label2.TabIndex = 2;
@@ -75,7 +87,7 @@ namespace Project_4
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(922, 467);
+            this.label3.Location = new System.Drawing.Point(146, 75);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 13);
             this.label3.TabIndex = 3;
@@ -84,7 +96,7 @@ namespace Project_4
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(922, 44);
+            this.label4.Location = new System.Drawing.Point(119, 75);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 13);
             this.label4.TabIndex = 4;
@@ -92,21 +104,25 @@ namespace Project_4
             // 
             // p1Score
             // 
-            this.p1Score.Location = new System.Drawing.Point(969, 44);
+            this.p1Score.Enabled = false;
+            this.p1Score.Location = new System.Drawing.Point(166, 72);
             this.p1Score.Name = "p1Score";
             this.p1Score.Size = new System.Drawing.Size(100, 20);
             this.p1Score.TabIndex = 5;
+            this.p1Score.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // p2Score
             // 
-            this.p2Score.Location = new System.Drawing.Point(969, 467);
+            this.p2Score.Enabled = false;
+            this.p2Score.Location = new System.Drawing.Point(202, 72);
             this.p2Score.Name = "p2Score";
             this.p2Score.Size = new System.Drawing.Size(100, 20);
             this.p2Score.TabIndex = 6;
+            this.p2Score.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // p2Roll
             // 
-            this.p2Roll.Location = new System.Drawing.Point(997, 519);
+            this.p2Roll.Location = new System.Drawing.Point(70, 206);
             this.p2Roll.Name = "p2Roll";
             this.p2Roll.Size = new System.Drawing.Size(190, 63);
             this.p2Roll.TabIndex = 7;
@@ -116,7 +132,7 @@ namespace Project_4
             // 
             // p1Roll
             // 
-            this.p1Roll.Location = new System.Drawing.Point(997, 99);
+            this.p1Roll.Location = new System.Drawing.Point(64, 206);
             this.p1Roll.Name = "p1Roll";
             this.p1Roll.Size = new System.Drawing.Size(190, 63);
             this.p1Roll.TabIndex = 8;
@@ -124,26 +140,94 @@ namespace Project_4
             this.p1Roll.UseVisualStyleBackColor = true;
             this.p1Roll.Click += new System.EventHandler(this.p1Roll_Click);
             // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(12, 328);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.p1Roll);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.p1Score);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.p2Roll);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.p2Score);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(752, 354);
+            this.splitContainer1.SplitterDistance = 370;
+            this.splitContainer1.TabIndex = 9;
+            // 
+            // gameStarter
+            // 
+            this.gameStarter.Location = new System.Drawing.Point(735, 12);
+            this.gameStarter.Name = "gameStarter";
+            this.gameStarter.Size = new System.Drawing.Size(75, 64);
+            this.gameStarter.TabIndex = 10;
+            this.gameStarter.Text = "Start Game?";
+            this.gameStarter.UseVisualStyleBackColor = true;
+            this.gameStarter.Click += new System.EventHandler(this.gameStarter_Click);
+            // 
+            // dice1Display
+            // 
+            this.dice1Display.Image = global::Project_4.Properties.Resources.Die6;
+            this.dice1Display.Location = new System.Drawing.Point(266, 191);
+            this.dice1Display.Name = "dice1Display";
+            this.dice1Display.Size = new System.Drawing.Size(107, 113);
+            this.dice1Display.TabIndex = 11;
+            this.dice1Display.TabStop = false;
+            this.dice1Display.Visible = false;
+            // 
+            // dice2Display
+            // 
+            this.dice2Display.Location = new System.Drawing.Point(392, 191);
+            this.dice2Display.Name = "dice2Display";
+            this.dice2Display.Size = new System.Drawing.Size(107, 113);
+            this.dice2Display.TabIndex = 12;
+            this.dice2Display.TabStop = false;
+            this.dice2Display.Visible = false;
+            // 
+            // turnMessage
+            // 
+            this.turnMessage.AutoSize = true;
+            this.turnMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.turnMessage.Location = new System.Drawing.Point(12, 20);
+            this.turnMessage.Name = "turnMessage";
+            this.turnMessage.Size = new System.Drawing.Size(102, 37);
+            this.turnMessage.TabIndex = 13;
+            this.turnMessage.Text = "label6";
+            this.turnMessage.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1390, 624);
-            this.Controls.Add(this.p1Roll);
-            this.Controls.Add(this.p2Roll);
-            this.Controls.Add(this.p2Score);
-            this.Controls.Add(this.p1Score);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(822, 722);
+            this.Controls.Add(this.turnMessage);
+            this.Controls.Add(this.dice2Display);
+            this.Controls.Add(this.dice1Display);
+            this.Controls.Add(this.gameStarter);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dice1Display)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dice2Display)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,6 +244,12 @@ namespace Project_4
         private System.Windows.Forms.TextBox p2Score;
         private System.Windows.Forms.Button p2Roll;
         private System.Windows.Forms.Button p1Roll;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button gameStarter;
+        private System.Windows.Forms.PictureBox dice1Display;
+        private System.Windows.Forms.PictureBox dice2Display;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label turnMessage;
     }
 }
 
--- Project 4/Project 4/Form1.cs
+++ Project 4/Project 4/Form1.cs
@@ -12,42 +12,190 @@ using System.Windows.Forms;
  * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/how-to-initialize-objects-by-using-an-object-initializer
  * https://zetcode.com/csharp/inttostring/
  * https://docs.microsoft.com/en-us/dotnet/api/system.random?view=net-5.0
- * 
- * 
+ * https://stackoverflow.com/questions/6560105/change-pictureboxs-image-to-image-from-my-resources
  */
 
 namespace Project_4
 {
     public partial class Form1 : Form
     {
+        //declare global variables
         player player1 = new player();
         player player2 = new player();
         int dice1Val, dice2Val;
+        Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+            p1Roll.Enabled = false;
+            p2Roll.Enabled = false;
             
+        }
+        private void p1Roll_Click(object sender, EventArgs e)
+        {
+            //initialize turn and roll dice
+            turnMessage.Visible = false;
+            diceRoll();
+            player1.increaseScore(dice1Val + dice2Val);
+
+            //display score
+            int p1ScorePlace;
+            p1ScorePlace = player1.getScore();
+            string outer = Convert.ToString(p1ScorePlace);
+            p1Score.Text = outer;
+
+            //check for doubles and respond accordingly
+            if (checkDubs())
+            {
+                turnMessage.Text = "Dubskie!!! Roll again!\nPlayer 1 Rolls Again";
+                turnMessage.Visible = true;
+            }
+            else
+            {
+                p1Roll.Enabled = false;
+                p2Roll.Enabled = true;
+            }
+
+            //check if win
+            if (player1.getScore() >= 50)
+            {
+                turnMessage.Text = "Hooray for Player 1! They Win!\nPress start to reset and play again.";
+                turnMessage.Visible = true;
+                p1Roll.Enabled = false;
+                p2Roll.Enabled = false;
+                gameStarter.Enabled = true;
+            }
         }
 
         private void p2Roll_Click(object sender, EventArgs e)
         {
+            //initialize turn and roll dice
+            turnMessage.Visible = false;
             diceRoll();
-            player1.increaseScore(dice1Val + dice2Val);
+            player2.increaseScore(dice1Val + dice2Val);
+
+            //display score
+            int p2ScorePlace;
+            p2ScorePlace = player2.getScore();
+            string outer = Convert.ToString(p2ScorePlace);
+            p2Score.Text = outer;
+
+            //check for doubles
+            if (checkDubs())
+            {
+                turnMessage.Text = "Wowsers, You got 2 of the same die!\nPlayer 2 Rolls Again";
+                turnMessage.Visible = true;
+            }
+            else
+            {
+                p1Roll.Enabled = true;
+                p2Roll.Enabled = false;
+            }
+            //check if win
+            if(player2.getScore() >= 50)
+            {
+                turnMessage.Text = "Hooray for Player 2! They Win!\nPress start to reset and play again.";
+                turnMessage.Visible = true;
+                p1Roll.Enabled = false;
+                p2Roll.Enabled = false;
+                gameStarter.Enabled = true;
+            }
         }
 
-        private void p1Roll_Click(object sender, EventArgs e)
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
         {
-            int test;
-            test = player1.getScore();
-            string outer = Convert.ToString(test);
-            p2Score.Text = outer;
+
+        }
+
+        private void gameStarter_Click(object sender, EventArgs e)
+        {
+            //initialize new game 
+            player1.resetScore();
+            player2.resetScore();
+            p1Score.Text = null;
+            p2Score.Text = null;
+            gameStarter.Enabled = false;
+
+            //randomly determine who goes first
+            //idk why it has to pull random number twice, but it doesn't work otherwise
+            int whoGoFirst = rand.Next(1, 3);
+            whoGoFirst = rand.Next(1, 3);
+            if(whoGoFirst < 2)
+            {
+                p1Roll.Enabled = true;
+            }
+            else
+            {
+                p2Roll.Enabled = true;
+            }
         }
 
         public void diceRoll()
         {
-            Random rand = new Random();
+            //generate dice 1 and display respective image
             dice1Val = rand.Next(1, 6);
+            switch (dice1Val)
+            {
+                case 1:
+                    dice1Display.Image = Project_4.Properties.Resources.Die1;
+                    break;
+                case 2:
+                    dice1Display.Image = Project_4.Properties.Resources.Die2;
+                    break;
+                case 3:
+                    dice1Display.Image = Project_4.Properties.Resources.Die3;
+                    break;
+                case 4:
+                    dice1Display.Image = Project_4.Properties.Resources.Die4;
+                    break;
+                case 5:
+                    dice1Display.Image = Project_4.Properties.Resources.Die5;
+                    break;
+                case 6:
+                    dice1Display.Image = Project_4.Properties.Resources.Die5;
+                    break;
+
+            }
+            dice1Display.Visible = true;
+
+            //generate dice 2 and display respective image
             dice2Val = rand.Next(1, 6);
+            switch (dice2Val)
+            {
+                case 1:
+                    dice2Display.Image = Project_4.Properties.Resources.Die1;
+                    break;
+                case 2:
+                    dice2Display.Image = Project_4.Properties.Resources.Die2;
+                    break;
+                case 3:
+                    dice2Display.Image = Project_4.Properties.Resources.Die3;
+                    break;
+                case 4:
+                    dice2Display.Image = Project_4.Properties.Resources.Die4;
+                    break;
+                case 5:
+                    dice2Display.Image = Project_4.Properties.Resources.Die5;
+                    break;
+                case 6:
+                    dice2Display.Image = Project_4.Properties.Resources.Die5;
+                    break;
+
+            }
+            dice2Display.Visible = true;
+        }
+
+        public bool checkDubs()
+        {
+            //check if dice 1 equals dice 2 and returns accordingly
+            if (dice1Val == dice2Val)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 
@@ -55,25 +203,34 @@ namespace Project_4
     {
         int score;
 
+        //starts player with a score of 0
         public player()
         {
             score = 0;
         }
+
+        //probably unneccissary since it wasn't utilized, but allows player to start with a specific score
         public player(int initScore)
         {
-            score = initScore;
+            score = initScore; 
         }
         
+
+        //increase score
         public void increaseScore(int rollAmount)
         {
             score += rollAmount;
         }
 
+
+        //returns player score
         public int getScore()
         {
             return score;
         }
 
+
+        //resets player score
         public void resetScore()
         {
             score = 0;
