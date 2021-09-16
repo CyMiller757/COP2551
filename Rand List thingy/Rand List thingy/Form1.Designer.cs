89abeac365a076a9fb987742ffa347475b7990ce
CyMiller757 <80051707+CyMiller757@users.noreply.github.com>
2021-09-16 10:43:38 -0400

    added in class project

---
+++ Rand List thingy/Rand List thingy/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Rand_List_thingy
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runButt = new System.Windows.Forms.Button();
+            this.inputThing = new System.Windows.Forms.TextBox();
+            this.outputData = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // runButt
+            // 
+            this.runButt.Location = new System.Drawing.Point(42, 231);
+            this.runButt.Name = "runButt";
+            this.runButt.Size = new System.Drawing.Size(254, 154);
+            this.runButt.TabIndex = 0;
+            this.runButt.Text = "Generate!";
+            this.runButt.UseVisualStyleBackColor = true;
+            this.runButt.Click += new System.EventHandler(this.runButt_Click);
+            // 
+            // inputThing
+            // 
+            this.inputThing.Location = new System.Drawing.Point(120, 147);
+            this.inputThing.Name = "inputThing";
+            this.inputThing.Size = new System.Drawing.Size(100, 23);
+            this.inputThing.TabIndex = 1;
+            // 
+            // outputData
+            // 
+            this.outputData.AutoSize = true;
+            this.outputData.Location = new System.Drawing.Point(517, 77);
+            this.outputData.Name = "outputData";
+            this.outputData.Size = new System.Drawing.Size(38, 15);
+            this.outputData.TabIndex = 2;
+            this.outputData.Text = "label1";
+            this.outputData.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputData);
+            this.Controls.Add(this.inputThing);
+            this.Controls.Add(this.runButt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runButt;
+        private System.Windows.Forms.TextBox inputThing;
+        private System.Windows.Forms.Label outputData;
+    }
+}
+
---
+++ Rand List thingy/Rand List thingy/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*Sources:
+ * https://www.guru99.com/c-sharp-stream.html
+ * 
+ * 
+ * 
+ * 
+ */
+
+namespace Rand_List_thingy
+{
+   
+    public partial class Form1 : Form
+    {
+
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void runButt_Click(object sender, EventArgs e)
+        {
+           try
+           {
+                int numsToGenerate;
+                int placeHolder;
+                string fileName = "randNumHolder.txt";
+                numsToGenerate = int.Parse(inputThing.Text);
+                File.Delete(fileName);
+                File.Create(fileName).Close();
+                using (StreamWriter data = File.AppendText(fileName))
+                {
+                    for (int i = 0; i < numsToGenerate; i++)
+                    {
+                     placeHolder = rand.Next(0, 100);
+                     data.WriteLine(placeHolder);
+
+                    }
+                }
+
+                string[] arrayHolder = File.ReadAllLines(fileName);
+                string outer = "";
+                for(int i = 0; i < arrayHolder.Length; i++)
+                {
+                    outer += arrayHolder[i] + "\n";
+                }
+                outputData.Text = outer;
+                outputData.Visible = true;
+                
+           }
+           catch
+           {
+            MessageBox.Show("Please enter correct input!");
+           }
+        }
+    }
+}
