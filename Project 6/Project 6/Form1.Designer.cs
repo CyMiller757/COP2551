e4b98b39b7ace711cb6688c9228bb83d70701a45
CyMiller757 <80051707+CyMiller757@users.noreply.github.com>
2021-10-13 16:06:08 -0400

    Project 6

---
+++ Project 6/Project 6/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Project_6
+{
+    partial class WorldSeries
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WorldSeries));
+            this.teamsBox = new System.Windows.Forms.ListBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.testText = new System.Windows.Forms.Label();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // teamsBox
+            // 
+            this.teamsBox.FormattingEnabled = true;
+            this.teamsBox.Location = new System.Drawing.Point(75, 48);
+            this.teamsBox.Name = "teamsBox";
+            this.teamsBox.Size = new System.Drawing.Size(120, 95);
+            this.teamsBox.TabIndex = 0;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(95, 159);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "Go!";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // testText
+            // 
+            this.testText.AutoSize = true;
+            this.testText.Location = new System.Drawing.Point(38, 9);
+            this.testText.Name = "testText";
+            this.testText.Size = new System.Drawing.Size(0, 13);
+            this.testText.TabIndex = 2;
+            this.testText.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(95, 197);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Quit";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // WorldSeries
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(278, 256);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.testText);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.teamsBox);
+            this.Name = "WorldSeries";
+            this.Text = "World Series";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox teamsBox;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Label testText;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ Project 6/Project 6/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*Sources used:
+ * https://stackoverflow.com/questions/23750228/use-an-array-to-assign-values-to-list-box
+ * https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/file-system/how-to-read-from-a-text-file
+ * 
+ */
+
+
+namespace Project_6
+{
+    public partial class WorldSeries : Form
+    {
+        //establish global variables
+        string[] teams = System.IO.File.ReadAllLines("Teams.txt");
+        string[] champs = System.IO.File.ReadAllLines("WorldSeries.txt");
+        public WorldSeries()
+        {
+            //add the Teams.txt info into the list box
+            InitializeComponent();
+            teamsBox.Items.AddRange(teams);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //check how many times the selected team has won the world series
+            int counter = 0;
+            if(teamsBox.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a team!");
+            }
+            else
+            {
+                foreach (string a in champs)
+                {
+                    if (teamsBox.SelectedItem.ToString() == a)
+                    {
+                        counter++;
+                    }
+                    testText.Text = "The " + teamsBox.SelectedItem + " have won " + counter.ToString() + " time(s)!";
+                }
+
+            }
+        }
+
+        //Riddle me this Batman, what could the following code possibly be used for? C'mon now Batman... THINK!
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
