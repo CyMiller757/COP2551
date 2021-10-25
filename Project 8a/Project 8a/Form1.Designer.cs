dd8a180a78908af0d74f968b5a979b6352441229
CyMiller757 <80051707+CyMiller757@users.noreply.github.com>
2021-10-25 13:45:21 -0400

    10/25

--- Project 8a/Project 8a/Form1.Designer.cs
+++ Project 8a/Project 8a/Form1.Designer.cs
@@ -30,32 +30,95 @@ namespace Project_8a
         private void InitializeComponent()
         {
             this.studentSelection = new System.Windows.Forms.ListBox();
+            this.searchInput = new System.Windows.Forms.TextBox();
+            this.searchOutput = new System.Windows.Forms.ListBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // studentSelection
             // 
             this.studentSelection.FormattingEnabled = true;
-            this.studentSelection.Location = new System.Drawing.Point(63, 48);
+            this.studentSelection.Location = new System.Drawing.Point(56, 91);
             this.studentSelection.Name = "studentSelection";
             this.studentSelection.Size = new System.Drawing.Size(118, 173);
             this.studentSelection.TabIndex = 0;
+            // 
+            // searchInput
+            // 
+            this.searchInput.Location = new System.Drawing.Point(262, 114);
+            this.searchInput.Name = "searchInput";
+            this.searchInput.Size = new System.Drawing.Size(120, 20);
+            this.searchInput.TabIndex = 1;
+            // 
+            // searchOutput
+            // 
+            this.searchOutput.FormattingEnabled = true;
+            this.searchOutput.Location = new System.Drawing.Point(262, 169);
+            this.searchOutput.Name = "searchOutput";
+            this.searchOutput.Size = new System.Drawing.Size(120, 95);
+            this.searchOutput.TabIndex = 2;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(262, 140);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(120, 23);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.BackColor = System.Drawing.Color.White;
+            this.searchLabel.Location = new System.Drawing.Point(244, 91);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(159, 13);
+            this.searchLabel.TabIndex = 4;
+            this.searchLabel.Text = "Search for students by birth year";
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(56, 270);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(326, 23);
+            this.Exit.TabIndex = 5;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.exit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::Project_8a.Properties.Resources.anton_sukhinov__C2A8THeKKs_unsplash_1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(451, 398);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.searchLabel);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchOutput);
+            this.Controls.Add(this.searchInput);
             this.Controls.Add(this.studentSelection);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox studentSelection;
+        private System.Windows.Forms.TextBox searchInput;
+        private System.Windows.Forms.ListBox searchOutput;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.Button Exit;
     }
 }
 
--- Project 8a/Project 8a/Form1.cs
+++ Project 8a/Project 8a/Form1.cs
@@ -19,6 +19,8 @@ namespace Project_8a
 {
     public partial class Form1 : Form
     {
+        //need this globablly for later purposes
+        Student[] peoples;
         public Form1()
         {
             InitializeComponent();
@@ -26,37 +28,86 @@ namespace Project_8a
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //establish variables and import txt file
             string[] nameAndYear = System.IO.File.ReadAllLines("Names.txt");
             int size = nameAndYear.Length, temp, year;
             string name;
+            Array.Sort(nameAndYear);
 
-            student[] peoples = new student[size];
+            //put the data into an array of student objects and add to the list
+            peoples = new Student[size];
             for (int i = 0; i < size; i++)
             {
                 temp = nameAndYear[i].Length - 4;
                 name = nameAndYear[i].Substring(0, temp);
                 year = int.Parse(nameAndYear[i].Substring(temp));
-                peoples[i] = new student(name, year);
+                peoples[i] = new Student(name, year);
                 studentSelection.Items.Add(name + year.ToString());
             }
         }
 
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            //establish variable and clear output incase of previous search
+            int search;
+            searchOutput.Items.Clear();
+
+            //try to turn string inputed into box into an integer
+            if(int.TryParse(searchInput.Text, out search))
+            {
+                //check through all students to find matching year and add to output if so
+                foreach (Student i in peoples)
+                {
+                    if(i.getYear() == search)
+                    {
+                        searchOutput.Items.Add(i.getName());
+                    }
+                }
+            }
+            //handle bad input
+            else
+            {
+                MessageBox.Show("Please enter a valid year in the search box!");
+            }
+
+            //handle no results found
+            if(searchOutput.Items.Count == 0)
+            {
+                searchOutput.Items.Add("No Matching");
+                searchOutput.Items.Add("Students found");
+            }
+        }
+
+        //the fun button
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
-    public class student
+
+
+    //students are in class
+    public class Student
     {
+        //declare variables associated with students
         private string name;
         private int year;
-        public student(string n, int y)
+
+        //initiator for students
+        public Student(string n, int y)
         {
             name = n;
             year = y;
         }
 
+        //return name
         public string getName()
         {
             return name;
         }
         
+
+        //return year of born
         public int getYear()
         {
             return year;
